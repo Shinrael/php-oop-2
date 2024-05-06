2d6bb41ff4217d09ead372691573338ae2522a6a
Shinrael <mircoelena89@gmail.com>
2024-05-06 17:57:27 +0200

    [add] try and catch

--- Model/Discounts.php
+++ Model/Discounts.php
@@ -4,7 +4,15 @@ trait Discount {
   public $discount;
   
   public function getDiscountPrice($discount){
-    $discountedPrice = $this->price * (1- $discount/100);
-    return $discountedPrice;
+    try{
+      if($discount < 0 || $discount > 100){
+        throw new Exception('Lo sconto deve essere compreso tra 0 e 100!');
+      }
+      $discountedPrice = $this->price * (1- $discount/100);
+      return $discountedPrice;
+    } catch(Exception $e){
+      var_dump($e->getMessage());
+    }
+    
   }
 }
